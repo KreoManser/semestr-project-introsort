1f1def5fd73af07973be30daaf8cc6a83a5fdec6
bulatsabirzyanov <sabirzyanov429@gmail.com>
2022-05-22 16:55:16 +0300

    s

--- dataset/generate_csv_dataset.cpp
+++ dataset/generate_csv_dataset.cpp
@@ -21,38 +21,91 @@ int main(int argc, char** argv) {
   const auto path = string(kDatasetPath);  // конвертация string_view в string
   cout << "Dataset path: " << path << endl;
 
-  // Пример: чтение набора данных
-  auto input_stream = ifstream(path + "/dataset-example.csv");
 
-  if (input_stream) {
-    int line_number = 0;
+  // Пример: генерация набора данных bad
+  for (int papka = 1; papka <= 5; papka++) {
+    string string_papka;
+    if (papka != 10) {
+      string_papka = "0" + to_string(papka);
+    } else {
+      string_papka = to_string(papka);
+    }
+    for (int size = 100; size < 1000001; size = size * 10) {
+      string string_size = to_string(size);
+      auto output_stream =
+          ofstream(path + "/" + "bad" + "/" + string_papka + "/" + string_size + ".csv", ios::ios_base::app);
 
-    // построчное чтение
-    for (string line; getline(input_stream, line); /* ... */) {
-      auto ss = stringstream(line);  // оборачиваем строку в объект "поток"
+      const auto seed = chrono::system_clock::now().time_since_epoch().count();
+      auto engine = mt19937(seed);  // без seed`а генератор будет выдавать одни и те же значения
+      auto dist = uniform_int_distribution(-100000, 100000);  // равновероятное распределение генерируемых чисел
 
-      for (string token; getline(ss, token, ','); /* ... */) {
-        cout << "Token: [" << token << "] at line " << line_number << '\n';
+      if (output_stream) {
+        for (int counter = 0; counter < size - 1; counter++) {
+          output_stream << dist(engine) << '\n';
+        }
+        output_stream << dist(engine) << '\n';
       }
+    }
+    for (int size = 500; size < 5000001; size = size * 10) {
+      string string_size = to_string(size);
+      auto output_stream =
+          ofstream(path + "/" + "bad" + "/" + string_papka + "/" + string_size + ".csv", ios::ios_base::app);
 
-      cout << line << '\n';
-      line_number++;
+      const auto seed = chrono::system_clock::now().time_since_epoch().count();
+      auto engine = mt19937(seed);  // без seed`а генератор будет выдавать одни и те же значения
+      auto dist = uniform_int_distribution(-100000, 100000);  // равновероятное распределение генерируемых чисел
+
+      if (output_stream) {
+        for (int counter = 0; counter < size - 1; counter++) {
+          output_stream << dist(engine) << '\n';
+        }
+        output_stream << dist(engine) << '\n';
+      }
     }
   }
-
-  // Пример: генерация набора данных
-  auto output_stream = ofstream(path + "/dataset-generated.csv", ios::ios_base::app);
-
-  const auto seed = chrono::system_clock::now().time_since_epoch().count();
-  auto engine = mt19937(seed);  // без seed`а генератор будет выдавать одни и те же значения
-  auto dist = uniform_int_distribution(0, 10);  // равновероятное распределение генерируемых чисел
-
-  if (output_stream) {
-    for (int counter = 0; counter < 10; counter++) {
-      output_stream << dist(engine) << ',';
+  // Пример: генерация набора данных good
+  for (int papka = 1; papka <= 5; papka++) {
+    string string_papka;
+    cout<<1;
+    if (papka != 10) {
+      string_papka = "0" + to_string(papka);
+    } else {
+      string_papka = to_string(papka);
     }
-    output_stream << dist(engine) << '\n';
-  }
+    for (int size = 100; size < 1000001; size = size * 10) {
+      string string_size = to_string(size);
+      auto output_stream =
+          ofstream(path + "/" + "good" + "/" + string_papka + "/" + string_size + ".csv", ios::ios_base::app);
 
+      const auto seed = chrono::system_clock::now().time_since_epoch().count();
+      auto engine = mt19937(seed);  // без seed`а генератор будет выдавать одни и те же значения
+      auto dist = uniform_int_distribution(-100000, 100000);  // равновероятное распределение генерируемых чисел
+      auto num = dist(engine);
+
+      if (output_stream) {
+        for (int counter = num; counter <= num + size - 1; counter++) {
+          output_stream << counter << '\n';
+        }
+      }
+      output_stream.close();
+    }
+    for (int size = 500; size < 500001; size = size * 10) {
+      string string_size = to_string(size);
+      auto output_stream =
+          ofstream(path + "/" + "good" + "/" + string_papka + "/" + string_size + ".csv", ios::ios_base::app);
+
+      const auto seed = chrono::system_clock::now().time_since_epoch().count();
+      auto engine = mt19937(seed);  // без seed`а генератор будет выдавать одни и те же значения
+      auto dist = uniform_int_distribution(-100000, 100000);  // равновероятное распределение генерируемых чисел
+      auto num = dist(engine);
+
+      if (output_stream) {
+        for (int counter = num; counter <= num + size - 1; counter++) {
+          output_stream << counter << '\n';
+        }
+      }
+      output_stream.close();
+    }
+  }
   return 0;
 }
